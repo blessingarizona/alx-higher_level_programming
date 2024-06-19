3d9ddf89ef4f5523a995938307172a3b188981c8
blessingarizona <blessingarinze9@gmail.com>
2024-06-19 14:24:21 +0100

    javascript

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
-// Script that prints three lines
-
-console.log("C is fun");
-console.log("Python is cool");
-console.log("JavaScript is amazing");
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
